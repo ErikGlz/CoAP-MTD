6428e1c679cb98ca8c9112842ffd75d36fc1ead2
Erik Ulises Gonzalez Gonzalez <81428381+ErikGlz@users.noreply.github.com>
2024-06-30 15:09:04 -0600

    Add files via upload

---
+++ Proyecto/dialects/coaprcs/server.py
@@ -0,0 +1,107 @@
+import logging
+import asyncio
+import hashlib
+import random
+from coaprcs import resource, Message, Context, error
+from coaprcs.numbers.codes import CHANGED, CONTENT
+from sense_hat import SenseHat
+import global_config
+from termcolor import colored
+
+def select_dialect(seed):
+    random.seed(seed)
+    dialect_number = random.randint(0, 4)
+    return dialect_number
+
+class TemperatureResource(resource.Resource):
+    """Resource that provides temperature readings from the SenseHat."""
+
+    def __init__(self):
+        super().__init__()
+        self.sense = SenseHat()
+
+    async def render_get(self, request):
+        # Verifica si la petición contiene payload
+        if request.payload:
+            print(colored(f"\nSEED RECEIVED","cyan"))
+
+            # Extrae la semilla del payload
+            seed = int(request.payload.decode('utf-8'))
+            new_dialect_index = select_dialect(seed)
+            global_config.selected_dialect_index = new_dialect_index
+            
+            print(colored(f"\nSWITCHING TO DIALECT: {global_config.selected_dialect_index+1}\n","light_magenta"))
+            global stop_server
+            stop_server = True
+
+            temperature = self.sense.get_temperature()
+            payload = f"Temperature: {temperature:.2f} C".encode('ascii')
+            
+            return Message(code=CONTENT, payload=payload)
+
+        else:
+            temperature = self.sense.get_temperature()
+            payload = f"Temperature: {temperature:.2f} C".encode('ascii')
+            return Message(code=CONTENT, payload=payload)
+
+class HumidityResource(resource.Resource):
+    """Resource that provides humidity readings from the SenseHat."""
+
+    def __init__(self):
+        super().__init__()
+        self.sense = SenseHat()
+
+    async def render_get(self, request):
+    # Verifica si la petición contiene payload
+        if request.payload:
+            print(colored(f"\nSEED RECEIVED","cyan"))
+
+            # Extrae la semilla del payload
+            seed = int(request.payload.decode('utf-8'))
+            new_dialect_index = select_dialect(seed)
+            global_config.selected_dialect_index = new_dialect_index
+
+            print(colored(f"\nSWITCHING TO DIALECT: {global_config.selected_dialect_index+1}\n","light_magenta"))
+            global stop_server
+            stop_server = True
+
+            temperature = self.sense.get_temperature()
+            payload = f"Temperature: {temperature:.2f} C".encode('ascii')
+            
+            return Message(code=CONTENT, payload=payload)
+
+        else:
+            humidity = self.sense.get_humidity()
+            payload = f"Humidity: {humidity:.2f}%".encode('ascii')
+            return Message(code=CONTENT, payload=payload)
+
+# logging setup
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("coap-server").setLevel(logging.DEBUG)
+
+async def main():
+    #Variable para controlar el ciclo del servidor
+    global stop_server
+    stop_server = False
+
+    # Resource tree creation
+    root = resource.Site()
+
+    root.add_resource(['temperature'], TemperatureResource())
+    root.add_resource(['humidity'], HumidityResource())
+
+    protocol = await Context.create_server_context(root)
+    print(colored("Server initialized\n","green"))
+
+    while not stop_server:
+        await asyncio.sleep(1)
+
+    #Detener el contexto actual
+    await Context.shutdown(protocol)
+    stop_server = False
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print(colored("Client stopped manually","light_red"))
---
+++ Proyecto/dialects/coapts/client.py
@@ -0,0 +1,134 @@
+import logging
+import asyncio
+import random
+import time
+from coapts import *
+import global_config
+from format_response import escape_special_chars, unscape_special_chars, create_response, fieldToNum, assignDialect, predecir_dialecto
+from termcolor import colored
+from rich.console import Console
+
+console = Console()
+
+logging.basicConfig(level=logging.INFO)
+
+def select_dialect(seed):
+    random.seed(seed)
+    dialect_number = random.randint(0, 4)
+    return dialect_number
+
+async def fetch_resource(protocol, uri, payload=None):
+    if payload:
+        request = Message(code=GET, uri=uri, payload=payload)
+        request.mtype = CON
+    else:
+        request = Message(code=GET, uri=uri)
+        request.mtype = CON
+
+    print(colored("\n--------------------------------------------------------------------------","green"))
+    print(colored('====== REQUEST ======',"green"))
+    print(f"Message type: {request.mtype}")
+    print(f"Message code: {request.code}")
+    print(f"Resource URI: {request.get_request_uri()}")
+    
+    try:
+        response = await protocol.request(request).response
+    except Exception as e:
+        print('\nFailed to fetch resource:')
+        print(e)
+    else:
+        #Formateo de la respuesta al formato del arbol
+        version = int(response.version)
+        tipo_nombre = str(response.mtype)
+        tipo_valor = int(response.mtype.value)
+        code = str(response.code)
+        payload = escape_special_chars(str(response.payload))
+        payload_length = len(payload)
+
+        cadena = create_response(version, tipo_nombre, tipo_valor, code, payload, payload_length)
+        cadena2 = cadena.replace('\t', ',')
+
+        print(colored('\n====== FORMATTING THE RECEIVED RESPONSE ======',"green"))
+        print('\nReceived response')
+        print(cadena2)
+
+        cadena_formated = fieldToNum(cadena)
+        print('\nFormatted response')
+        print(cadena_formated)
+        
+        respuesta_final = assignDialect(cadena_formated)
+        print('\nFinal response for the decision tree')
+        print(respuesta_final)
+
+        #Introducir la respuesta al arbol
+        dialecto_predicho = predecir_dialecto(respuesta_final)
+
+        if dialecto_predicho != 0 and dialecto_predicho == global_config.selected_dialect_index+1:
+            print(colored(f"\nDIALECT DETECTED: {dialecto_predicho}","cyan"))
+            print(colored('\n====== RESPONSE ======',"green"))
+            print(f"Version: {response.version}")
+            print(f"Message type: {response.mtype}")
+            print(f"Message type code: {response.mtype.value}")
+            print(f"Message TKL: {len(response.token)}")
+            print(f"Response code: {response.code}")
+            print(f"Message ID: {response.mid}")
+            print(f"Message Token: {response.token.hex()}")
+            print(f"Payload: {response.payload.decode('utf-8')}")
+            print(colored("--------------------------------------------------------------------------","green"))
+    
+        else:
+            print(colored("\nMALICIOUS RESPONSE DETECTED","red"))
+            print(colored("Ignoring info","red"))
+            print(colored("--------------------------------------------------------------------------","red"))
+
+async def main():
+    global stop_client
+    stop_client = False
+
+    while not stop_client:
+        #Crear contexto del cliente del dialecto en uso
+        protocol = await Context.create_client_context()
+
+        #Seleccionar tiempo en el que se usa el dialecto
+        dialect_duration = random.randint(5, 20)
+        end_time = time.time() + dialect_duration
+        print(colored(f"Using dialect for {dialect_duration} seconds","yellow"))
+        print(colored("--------------------------------------------------------------------------","yellow"))
+
+        #Uso del dialecto dentro del tiempo de uso
+        while time.time() < end_time:
+            uri_input = input(colored("\nEnter the full URI of the resource: ","light_yellow"))
+            if uri_input.lower() == 'exit':
+                break
+
+            await fetch_resource(protocol, uri_input)
+            await asyncio.sleep(1)
+
+        #Generar seed para seleccionar el nuevo dialecto
+        new_seed = random.randint(0, 100000)
+        new_dialect_index = select_dialect(new_seed)
+
+        #Asegura que el nuevo dialecto seleccionado no coincida con el que está en uso
+        while global_config.selected_dialect_index == new_dialect_index:
+            new_seed = random.randint(0, 100000)
+            new_dialect_index = select_dialect(new_seed)
+
+        # Enviar una petición para cambiar el dialecto
+        print(colored(f"\nSENDING SEED...","light_grey"))
+        payload = str(new_seed).encode('utf-8')
+        uri_input = "coap://192.168.0.115/temperature"
+        await fetch_resource(protocol, uri_input, payload=payload)
+
+        #Actualiza el dialecto a utilizar
+        global_config.selected_dialect_index = new_dialect_index
+        print(colored(f"\n\nSWITCHING TO DIALECT: {global_config.selected_dialect_index+1}","light_magenta"))
+
+        #Finaliza el contexto del cliente del dialecto en uso
+        stop_client = True
+        await Context.shutdown(protocol)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Client stopped manually")
